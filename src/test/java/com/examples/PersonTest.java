294ac95423ca52f09c328cf18385963f3dc557a3
saiki <saiki@SAIKIRAN>
2018-02-27 10:40:22 -0500

    Intital push

--- src/test/java/com/examples/PersonTest.java
+++ src/test/java/com/examples/PersonTest.java
@@ -4,11 +4,34 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import org.junit.Test;
+
+import org.junit.Test;
+
 public class PersonTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testPerson() {
+		Person p1 = new Person();
+		p1.setName("John Doe");
+		assertEqauls("John Doe", p1.getName());
+		
+	}
+	
+	@Test
+	public void testNoOfBooks() {
+		Person p2 = new Person();
+		p2.setname("Jane Doe");
+		p2.setNoOfBooks(5);
+		assertEquals(5, p2.getNoOfBooks());
+	}
+	
+	@Test
+	public void testToString() {
+		Person p3 = new Person();
+		p3.setName("Java Guru");
+		p3.setNoOfBooks(10);
+		assertEquals("Java Guru has : (10 books)",p3.toString());
 	}
 
 }
